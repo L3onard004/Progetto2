2835e2033f33821f2d9edaa6e7b841c1f9bf6ebe
L3onard004 <leonardo.lorenzin.1@studenti.unipd.it>
2025-05-03 12:54:40 +0200

    Test3
    close #5

--- src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -2,3 +2,18 @@
 // Leonardo Lorenzin 2116434
 // Gianluca Bacco 2111937
 ////////////////////////////////////////////////////////////////////
+package it.unipd.mtss;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class IntegerToRomanTest {
+    @Test
+    public void testConvert() {
+        assertEquals("I", IntegerToRoman.convert(1));
+        assertEquals("II", IntegerToRoman.convert(2));
+        assertEquals("III", IntegerToRoman.convert(3));
+
+    }
+}
